239130b834ba8a4026e4f460eb83a0907ce4d62e
tiagoc0sta <tiagodpv@gmail.com>
2024-06-04 09:59:55 -0400

    f

---
+++ Vollmed/Services/WebService.swift
@@ -0,0 +1,27 @@
+//
+//  WebService.swift
+//  Vollmed
+//
+//  Created by Giovanna Moeller on 12/09/23.
+//
+
+import Foundation
+
+struct WebService {
+    
+    private let baseURL = "http://localhost:3000"
+    
+    func getAllSpecialists() async throws -> [Specialist]? {
+        let endpoint = baseURL + "/especialista"
+        
+        guard let url = URL (string: endpoint) else {
+            print("Erro na URL!")
+            return nil
+        }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return specialists
+    }
+}
